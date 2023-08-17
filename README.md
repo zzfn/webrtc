cdfd3812b1e289112adbd073a3946f24f5df2763
orluna <44751363+zzfn@users.noreply.github.com>
2023-08-17 19:44:27 +0800

    f

---
+++ README.md
@@ -0,0 +1,21 @@
+# WebRTC 30 分钟快速实践
+
+React Hooks + TypeScript
+
+Bilibili 视频：[https://www.bilibili.com/video/BV1W14y1Y7fQ/](https://www.bilibili.com/video/BV1W14y1Y7fQ/)
+
+# 初始化
+
+```bash
+yarn
+```
+
+# 运行
+
+```bash
+yarn dev
+```
+
+# 开源协议
+
+[GNU Affero General Public License v3.0](https://choosealicense.com/licenses/agpl-3.0/)
---
+++ src/App.tsx
@@ -0,0 +1,153 @@
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
+
+function App() {
+  const localVideoRef = useRef<HTMLVideoElement>(null)
+  const remoteVideoRef = useRef<HTMLVideoElement>(null)
+  const pc = useRef<RTCPeerConnection>()
+  const localStreamRef = useRef<MediaStream>()
+  const wsRef = useRef(new WebSocket('wss://ws.zzfzzf.com'))
+  const username = (Math.random() + 1).toString(36).substring(7)
+  const [status, setStatus] = useState('开始通话')
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(() => {
+      initWs()
+      getMediaDevices().then(() => {
+        createRtcConnection()
+        addLocalStreamToRtcConnection()
+      })
+    })
+  }, [])
+
+  const initWs = () => {
+    wsRef.current.onopen = () => console.log('ws 已经打开')
+    wsRef.current.onmessage = wsOnMessage
+  }
+
+  const wsOnMessage = (e:any) => {
+    // e.data.text().then((msg: any) => {
+    console.log(11111111, e, JSON.parse(e.data))
+    const wsData = JSON.parse(e.data);
+      console.log('wsData', wsData)
+
+      const wsUsername = wsData['username']
+      console.log('wsUsername', wsUsername)
+      if (username === wsUsername) {
+        console.log('跳过处理本条消息')
+        return
+      }
+
+      const wsType = wsData['type']
+      console.log('wsType', wsType)
+
+      if (wsType === 'offer') {
+        const wsOffer = wsData['data']
+        pc.current?.setRemoteDescription(new RTCSessionDescription(JSON.parse(wsOffer)))
+        setStatus('请接听通话')
+      }
+      if (wsType === 'answer') {
+        const wsAnswer = wsData['data']
+        pc.current?.setRemoteDescription(new RTCSessionDescription(JSON.parse(wsAnswer)))
+        setStatus('通话中')
+      }
+      if (wsType === 'candidate') {
+        const wsCandidate = JSON.parse(wsData['data'])
+        pc.current?.addIceCandidate(new RTCIceCandidate(wsCandidate))
+        console.log('添加候选成功', wsCandidate)
+      }
+    // })
+  }
+
+  const wsSend = (type: string, data: any) => {
+    wsRef.current.send(JSON.stringify({
+      username,
+      type,
+      data,
+    }))
+  }
+
+  const getMediaDevices = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    })
+    console.log('stream', stream)
+    localVideoRef.current!.srcObject = stream
+    localStreamRef.current = stream
+  }
+
+  const createRtcConnection = () => {
+    const _pc = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: ['stun:stun.stunprotocol.org:3478'],
+        }
+      ]
+    })
+    _pc.onicecandidate = e => {
+      if (e.candidate) {
+        console.log('candidate', JSON.stringify(e.candidate))
+        wsSend('candidate', JSON.stringify(e.candidate))
+      }
+    }
+    _pc.ontrack = e => {
+      remoteVideoRef.current!.srcObject = e.streams[0]
+    }
+    pc.current = _pc
+    console.log('rtc 连接创建成功', _pc)
+  }
+
+  const createOffer = () => {
+    pc.current?.createOffer({
+      offerToReceiveVideo: true,
+      offerToReceiveAudio: true,
+    })
+      .then(sdp => {
+        console.log('offer', JSON.stringify(sdp))
+        pc.current?.setLocalDescription(sdp)
+        wsSend('offer', JSON.stringify(sdp))
+        setStatus('等待对方接听')
+      })
+  }
+
+  const createAnswer = () => {
+    pc.current?.createAnswer({
+      offerToReceiveVideo: true,
+      offerToReceiveAudio: true,
+    })
+      .then(sdp => {
+        console.log('answer', JSON.stringify(sdp))
+        pc.current?.setLocalDescription(sdp)
+        wsSend('answer', JSON.stringify(sdp))
+        setStatus('通话中')
+      })
+  }
+
+  const addLocalStreamToRtcConnection = () => {
+    const localStream = localStreamRef.current!
+    localStream.getTracks().forEach(track => {
+      pc.current!.addTrack(track, localStream)
+    })
+    console.log('将本地视频流添加到 RTC 连接成功')
+  }
+
+  return (
+    <div>
+      <div>{`username:${username}`}</div>
+      <video style={{ width: '400px' }} ref={localVideoRef} autoPlay controls></video>
+      <video style={{ width: '400px' }} ref={remoteVideoRef} autoPlay controls></video>
+      <br />
+      <p>{`当前状态：${status}`}</p>
+      <br />
+      {status === '开始通话' && (
+        <button onClick={createOffer}>拨号</button>
+      )}
+      {status === '请接听通话' && (
+        <button onClick={createAnswer}>接听</button>
+      )}
+    </div>
+  )
+}
+
+export default App
